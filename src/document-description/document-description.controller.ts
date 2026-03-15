be9f75eb9b30d0c176ed5415603fc71ccea9f827
Alex Sokolskikh <you@example.com>
2026-03-15 16:42:56 +0300

    added controller and service update seed

--- src/document-description/document-description.controller.ts
+++ src/document-description/document-description.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { DocumentDescriptionService } from './document-description.service';
 
-@Controller('document-description')
-export class DocumentDescriptionController {}
+@Controller('document-descriptions')
+export class DocumentDescriptionController {
+  constructor(
+    private readonly documentDescriptionService: DocumentDescriptionService,
+  ) {}
+
+  @Get()
+  findAll() {
+    return this.documentDescriptionService.findAll();
+  }
+
+  @Get(':uuid')
+  findOne(@Param('uuid') uuid: string) {
+    return this.documentDescriptionService.findOne(uuid);
+  }
+}
--- src/document-description/document-description.service.ts
+++ src/document-description/document-description.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { DocumentDescription } from './document-description.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class DocumentDescriptionService {}
+export class DocumentDescriptionService {
+  constructor(
+    @InjectRepository(DocumentDescription)
+    private documentDescriptionRepository: Repository<DocumentDescription>,
+  ) {}
+
+  async findAll(): Promise<DocumentDescription[]> {
+    return this.documentDescriptionRepository.find();
+  }
+
+  async findOne(uuid: string): Promise<DocumentDescription | null> {
+    return this.documentDescriptionRepository.findOne({ where: { uuid } });
+  }
+}
